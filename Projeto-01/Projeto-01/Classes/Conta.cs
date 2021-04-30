65e8921df52846b0396398f5aa7f88eca1b409e7
Sagex <jones.kapo@hotmail.com>
2021-04-30 17:26:19 -0300

    Projeto 01 csharp de um mini app para transferencias com dotnet

---
+++ Projeto-01/Projeto-01/Classes/Conta.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto_01.Enum;
+
+namespace Projeto_01.Classes
+{
+    class Conta
+    {
+        private TipoDeConta _tipoDeConta { get; set; }
+
+        private string _nome { get; set; }
+
+        private double _saldo { get; set; }
+
+        private double _credito { get; set; }
+
+        public Conta(TipoDeConta tipoDeConta, string nome, double saldo, double credito)
+        {
+            this._tipoDeConta = tipoDeConta;
+            this._nome = nome;
+            this._saldo = saldo;
+            this._credito = credito;
+        }
+
+        public bool Sacar(double valorDoSaque)
+        {
+            if (this._saldo - valorDoSaque < this._credito * -1)
+            {
+                Console.WriteLine("Voce nao possui saldo. Insira um novo valor.");
+                return false;
+            }
+
+            this._saldo -= valorDoSaque;
+            Console.WriteLine("Saque realizado com sucesso.");
+            Console.WriteLine($"Seu saldo atual da conta: Conta: {this._tipoDeConta} | {this._nome} | {this._saldo}");
+
+            return true;
+        }
+
+        public void Depositar(double valorDoDeposito)
+        {
+            this._saldo += valorDoDeposito;
+
+            Console.WriteLine("Valor depositado com sucesso.");
+            Console.WriteLine($"Seu saldo atual da conta: Conta: {this._tipoDeConta} | {this._nome} | {this._saldo}");
+        }
+
+        public void Transferir(double valorDaTransferencia, Conta destino)
+        {
+            if (this.Sacar(valorDaTransferencia))
+            {
+                destino.Depositar(valorDaTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string _tipoTemp;
+
+            if (ReturnTipoConta() == "PessoaFisica")
+            {
+                _tipoTemp = "Pessoa Fisica";
+            }
+            else
+            {
+                _tipoTemp = "Pessoa Juridica";
+            }
+
+            string dados = $"Cliente: {_nome} || Tipo: {_tipoTemp} || Saldo: {_saldo} || Credito: {_credito}";
+
+            return dados;
+        }
+
+        private string ReturnTipoConta()
+        {
+            string tempNome = _tipoDeConta.ToString();
+            return tempNome;
+        }
+
+    }
+
+    
+}
---
+++ Projeto-01/Projeto-01/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using Projeto_01.Classes;
+using Projeto_01.Enum;
+
+
+namespace Projeto_01
+{
+    class Program
+    {
+        static List<Conta> _usuario = new();
+        static void Main(string[] args)
+        {               
+
+            string _opcaoUsuario = ObterOpcaoUsuario();
+            
+            while(_opcaoUsuario != "X")
+            {
+                switch (_opcaoUsuario)
+                {
+                    case "1":
+                        Lista();
+                        break;
+                    case "2":
+                        InserirNovaConta();
+                        break;
+                        Console.WriteLine();
+                    case "3":
+                        TransferirValor();
+                        break;
+                    case "4":
+                        RetirarValor();
+                        break;
+                    case "5":
+                        DepositarValor();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                _opcaoUsuario = ObterOpcaoUsuario();
+            }
+            
+        }
+
+
+        private static void Lista()
+        {
+            int _tempCount = 0;
+            if(_usuario.Count == 0)
+            {
+                Console.WriteLine("Não existe conta(as) cadastradas");
+                Console.WriteLine();
+                return;
+            }
+            foreach (var x in _usuario)
+            {
+                Console.WriteLine($"ID: #{_tempCount} | {x.ToString()}");
+                Console.WriteLine();
+                Console.WriteLine("===========================================");
+                Console.WriteLine();
+                _tempCount++;
+            }
+            
+        }
+
+        private static void InserirNovaConta()
+        {
+            Console.WriteLine("Insida os dados pedidos abaixo: ");
+
+            Console.Write("Tipo da conta, 1 fisica, 2 juridica: \n");
+            int _tipoTemp = int.Parse(Console.ReadLine());
+
+            Console.Write("Nome: \n");
+            string _nomeTemp = Console.ReadLine();
+
+            Console.Write("Saldo: \n");
+            double _tempSaldo = double.Parse(Console.ReadLine());
+
+            Console.Write("Credito: \n");
+            double _tempCredito = double.Parse(Console.ReadLine());
+
+            _usuario.Add(new Conta((TipoDeConta)_tipoTemp, _nomeTemp, _tempSaldo, _tempCredito));
+
+            Console.WriteLine("Novo usuario adicionado.");
+            Console.WriteLine();
+        }
+
+        private static void TransferirValor()
+        {
+            int _idTemporario;
+            int _idDestinoTemporario;
+            double _valorTemporario;
+
+            Console.WriteLine();
+            Lista();
+            Console.WriteLine("Insira o ID que da conta a ser retirada: ");
+            _idTemporario = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Agora, digite o valor a ser transferido: ");
+            _valorTemporario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Agora, para qual conta deseja transferir?: ");
+            _idDestinoTemporario = int.Parse(Console.ReadLine());
+
+            _usuario[_idTemporario].Transferir(_valorTemporario, _usuario[_idDestinoTemporario]);
+        }
+
+        private static void RetirarValor()
+        {
+            int _idTemporario;
+            double _valorTemporario;
+            Console.WriteLine();
+            Lista();
+            Console.WriteLine("Insira o ID que da conta a ser retirada: ");
+            _idTemporario = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Agora, digite o valor a ser retirado: ");
+            _valorTemporario = double.Parse(Console.ReadLine());
+
+            _usuario[_idTemporario].Sacar(_valorTemporario);
+        }
+
+        private static void DepositarValor()
+        {
+            int _idTemporario;
+            double _valorTemporario;
+            Console.WriteLine();
+            Lista();
+            Console.WriteLine("Insira o ID que da conta a ser depositada: ");
+            _idTemporario = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Agora, digite o valor a ser depositado: ");
+            _valorTemporario = double.Parse(Console.ReadLine());
+
+            _usuario[_idTemporario].Depositar(_valorTemporario);
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opcao desejada: ");
+            Console.WriteLine();
+            Console.WriteLine("1 - Listar Contas");
+            Console.WriteLine("2 - Inserir Conta");
+            Console.WriteLine("3 - Transferir");
+            Console.WriteLine("4 - Sacar");
+            Console.WriteLine("5 - Depositar");
+            Console.WriteLine("C - Limpar Tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string _opcao = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+
+            return _opcao;
+        }
+    }
+}
